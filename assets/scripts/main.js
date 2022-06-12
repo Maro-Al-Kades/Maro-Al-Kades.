1f24263a570e85733db1a3c9c595890163c6ddd5
Maro-Al-Kades <97260148+Maro-Al-Kades@users.noreply.github.com>
2022-06-12 03:02:34 +0200

    Add files via upload

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+<section style="max-height: 100%; margin: auto;">
+<div id="ripple" style="height: 70rem; 
+background:transparent;">
+</div>
+</section>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+<script src='https://cdn.jsdelivr.net/npm/jquery.ripples@0.6.3/dist/jquery.ripples.min.js'></script>
+<script  src="./script.js"></script>
